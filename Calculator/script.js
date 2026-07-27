12e49418db4a08580ad2cb7395d231885671c6d9
ziggykilei1-arch <ziggykilei1@gmail.com>
2026-07-27 11:01:52 +0300

    new

---
+++ Calculator/script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById('display');
+function appendToDisplay(value) {
+    display.value += value;
+}
+function clearDisplay() {
+    display.value = '';
+}
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
